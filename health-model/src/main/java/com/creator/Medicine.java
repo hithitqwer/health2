007833978cfcc3e7765f3971ea53c9417277308f
zhangzeyu003 <zhangzeyu003@lianjia.com>
2018-07-10 19:07:35 +0800

    20180710

---
+++ health-model/src/main/java/com/creator/Medicine.java
@@ -0,0 +1,13 @@
+package com.creator;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+@ToString
+public class Medicine implements Serializable {
+}
